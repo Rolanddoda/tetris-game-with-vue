76794c18e144121204c8a874f3b6562a95c37ab5
Roland1993 <roland.buzzooka@gmail.com>
2018-11-08 09:05:13 +0100

    Tetris game T item

---
+++ src/components/Tetris/tetris_items.js
@@ -0,0 +1,90 @@
+export const tetris_items = [
+
+    {
+        type: 'T',
+        value: [
+            [0, 0, 0, 1, 1, 1, 0, 0, 0],
+            [0, 0, 0, 0, 1, 0, 0, 0, 0],
+        ],
+        row_1_starts: 3,
+        row_1_ends: 5,
+        row_2_starts: 4,
+        row_2_ends: 4,
+    },
+
+    {
+        type: 'Z',
+        value: [
+            [0, 0, 0, 1, 1, 0, 0, 0, 0],
+            [0, 0, 0, 0, 1, 1, 0, 0, 0],
+        ],
+        row_1_starts: 3,
+        row_1_ends: 4,
+        row_2_starts: 4,
+        row_2_ends: 5,
+    },
+
+    {
+        type: 'S',
+        value: [
+            [0, 0, 0, 0, 1, 1, 0, 0, 0],
+            [0, 0, 0, 1, 1, 0, 0, 0, 0],
+        ],
+        row_1_starts: 4,
+        row_1_ends: 5,
+        row_2_starts: 3,
+        row_2_ends: 4,
+    },
+
+    {
+        type: 'I',
+        value: [
+            [0, 0, 0, 0, 1, 0, 0, 0, 0],
+            [0, 0, 0, 0, 1, 0, 0, 0, 0],
+            [0, 0, 0, 0, 1, 0, 0, 0, 0],
+        ],
+        row_1_starts: 4,
+        row_1_ends: 4,
+        row_2_starts: 4,
+        row_2_ends: 4,
+        row_3_starts: 4,
+        row_4_ends: 4,
+    },
+
+    { //I but horizontal
+        type: 'IH',
+        value: [
+            [0, 0, 0, 1, 1, 1, 0, 0, 0],
+        ],
+        row_1_starts: 3,
+        row_2_ends: 5,
+    },
+
+    {
+        type: '0',
+        value: [
+            [0, 0, 0, 0, 1, 1, 0, 0, 0],
+            [0, 0, 0, 0, 1, 1, 0, 0, 0],
+        ],
+        row_1_starts: 4,
+        row_1_ends: 5,
+        row_2_starts: 4,
+        row_2_ends: 5,
+    },
+
+    {
+        type: 'L',
+        value: [
+            [0, 0, 0, 1, 0, 0, 0, 0, 0],
+            [0, 0, 0, 1, 0, 0, 0, 0, 0],
+            [0, 0, 0, 1, 1, 0, 0, 0, 0],
+        ],
+        row_1_starts: 3,
+        row_1_ends: 3,
+        row_2_starts: 3,
+        row_2_ends: 3,
+        row_3_starts: 3,
+        row_3_ends: 4,
+    },
+
+]
