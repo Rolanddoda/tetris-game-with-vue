88c139b5346cf9df27e9dfd0087eb0a83afeccec
Roland1993 <roland.buzzooka@gmail.com>
2018-11-13 11:35:29 +0100

    Initialize tetris game (functional)

--- src/components/Tetris/tetris_items.js
+++ src/components/Tetris/tetris_items.js
@@ -1,90 +1,102 @@
 export const tetris_items = [
 
-    {
-        type: 'T',
-        value: [
-            [0, 0, 0, 1, 1, 1, 0, 0, 0],
-            [0, 0, 0, 0, 1, 0, 0, 0, 0],
-        ],
-        row_1_starts: 3,
-        row_1_ends: 5,
-        row_2_starts: 4,
-        row_2_ends: 4,
-    },
+	{
+		type: "T",
+		value: [
+		  [0, 0, 0, 2, 2, 2, 0, 0, 0],
+      [0, 0, 0, 0, 2, 0, 0, 0, 0]
+    ],
+		rows_count: 2,
+		matrix: [[2, 2, 2], [0, 2, 0]],
+		starts: 3, //start shows the position of tetris can be [0,1,2,3,4,5,6]
+		expands: 3, //how many rows the tetris expanded
+		rotated: false,
+		is_rotatable: true
+	},
 
-    {
-        type: 'Z',
-        value: [
-            [0, 0, 0, 1, 1, 0, 0, 0, 0],
-            [0, 0, 0, 0, 1, 1, 0, 0, 0],
-        ],
-        row_1_starts: 3,
-        row_1_ends: 4,
-        row_2_starts: 4,
-        row_2_ends: 5,
-    },
+	{
+		type: 'Z',
+		value: [
+		  [0, 0, 0, 2, 2, 0, 0, 0, 0],
+      [0, 0, 0, 0, 2, 2, 0, 0, 0]
+    ],
+    rows_count: 2,
+    matrix: [[2, 2, 0], [0, 2, 2]],
+    starts: 3,
+    expands: 3,
+    rotated: false,
+		is_rotatable: true
+	},
 
-    {
-        type: 'S',
-        value: [
-            [0, 0, 0, 0, 1, 1, 0, 0, 0],
-            [0, 0, 0, 1, 1, 0, 0, 0, 0],
-        ],
-        row_1_starts: 4,
-        row_1_ends: 5,
-        row_2_starts: 3,
-        row_2_ends: 4,
-    },
+	{
+		type: 'S',
+		value: [
+			[0, 0, 0, 0, 2, 2, 0, 0, 0],
+			[0, 0, 0, 2, 2, 0, 0, 0, 0],
+		],
+		rows_count: 2,
+		matrix: [[0, 2, 2], [2, 2, 0]],
+		starts: 3,
+		expands: 3,
+		rotated: false,
+		is_rotatable: true
+	},
 
-    {
-        type: 'I',
-        value: [
-            [0, 0, 0, 0, 1, 0, 0, 0, 0],
-            [0, 0, 0, 0, 1, 0, 0, 0, 0],
-            [0, 0, 0, 0, 1, 0, 0, 0, 0],
-        ],
-        row_1_starts: 4,
-        row_1_ends: 4,
-        row_2_starts: 4,
-        row_2_ends: 4,
-        row_3_starts: 4,
-        row_4_ends: 4,
-    },
+	{
+		type: 'I',
+		value: [
+			[0, 0, 0, 0, 2, 0, 0, 0, 0],
+			[0, 0, 0, 0, 2, 0, 0, 0, 0],
+			[0, 0, 0, 0, 2, 0, 0, 0, 0],
+		],
+		rows_count: 3,
+		matrix: [[0, 2, 0], [0, 2, 0], [0, 2, 0]],
+		starts: 4,
+		expands: 1,
+		rotated: false,
+		is_rotatable: true
+	},
 
-    { //I but horizontal
-        type: 'IH',
-        value: [
-            [0, 0, 0, 1, 1, 1, 0, 0, 0],
-        ],
-        row_1_starts: 3,
-        row_2_ends: 5,
-    },
+	{ //I but horizontal
+		type: 'IH',
+		value: [
+			[0, 0, 0, 2, 2, 2, 0, 0, 0],
+		],
+		rows_count: 1,
+		matrix: [[0, 0, 0], [2, 2, 2], [0, 0, 0]],
+		starts: 3,
+		expands: 3,
+		rotated: false,
+		is_rotatable: true
+	},
 
-    {
-        type: '0',
-        value: [
-            [0, 0, 0, 0, 1, 1, 0, 0, 0],
-            [0, 0, 0, 0, 1, 1, 0, 0, 0],
-        ],
-        row_1_starts: 4,
-        row_1_ends: 5,
-        row_2_starts: 4,
-        row_2_ends: 5,
-    },
+	{
+		type: 'O',
+		value: [
+			[0, 0, 0, 0, 2, 2, 0, 0, 0],
+			[0, 0, 0, 0, 2, 2, 0, 0, 0],
+		],
+		rows_count: 2,
+		matrix: [[0, 2, 2], [0, 2, 2]],
+		starts: 4,
+		expands: 2,
+		rotated: false,
+		is_rotatable: false
+	},
 
-    {
-        type: 'L',
-        value: [
-            [0, 0, 0, 1, 0, 0, 0, 0, 0],
-            [0, 0, 0, 1, 0, 0, 0, 0, 0],
-            [0, 0, 0, 1, 1, 0, 0, 0, 0],
-        ],
-        row_1_starts: 3,
-        row_1_ends: 3,
-        row_2_starts: 3,
-        row_2_ends: 3,
-        row_3_starts: 3,
-        row_3_ends: 4,
-    },
+	{
+		type: 'L',
+		value: [
+			[0, 0, 0, 2, 0, 0, 0, 0, 0],
+			[0, 0, 0, 2, 0, 0, 0, 0, 0],
+			[0, 0, 0, 2, 2, 0, 0, 0, 0],
+		],
+		rows_count: 3,
+		matrix: [[2, 0, 0], [2, 0, 0], [2, 2, 0]],
+		starts: 3,
+		expands: 2,
+		rotated: false,
+		is_rotatable: true
+	},
 
 ]
